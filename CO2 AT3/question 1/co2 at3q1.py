c4bf7851d301309df5947aa4354a6aa26dfd152d
dillibabu1122 <192472284.simats@saveetha.com>
2026-06-24 10:22:28 +0530

    Add files via upload

---
+++ CO2 AT3/question 1/co2 at3q1.py
@@ -0,0 +1,17 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        # Insert key in correct position
+        arr[j + 1] = key
+
+    return arr
+arr = [12, 11, 13, 5, 6]
+print("Original Array:", arr)
+sorted_arr = insertion_sort(arr)
+print("Sorted Array:", sorted_arr)
+
